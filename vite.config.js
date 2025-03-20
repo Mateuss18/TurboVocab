8b1ddd3a2317f4a64e8f417ec743aee1857c67bb
Mateuss18 <mateusdfl18@gmail.com>
2025-03-19 23:15:47 -0300

    feat: proxy config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      // Todas as chamadas para "/api" serão redirecionadas para a API Inference do Hugging Face
+      '/api': {
+        target: 'https://api-inference.huggingface.co',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
