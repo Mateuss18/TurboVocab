ea4b9ac404505cd1638dea78a0cff6674e200f28
Mateuss18 <mateusdfl18@gmail.com>
2025-03-25 23:22:03 -0300

    feat: renaming files

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
-<script setup>
-import MainContent from './components/MainContent.vue';
-import TesteAI from './components/TesteAI.vue';
-</script>
-
 <template>
   <TesteAI/>
 </template>
+
+<script setup>
+// import MainContent from './components/MainContent.vue';
+import TesteAI from './components/TesteWebScraping.vue';
+</script>
 
 <style scoped>
 </style>
