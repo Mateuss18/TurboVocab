4b27dcb9e158d76b9fdb999e834f85430aa0777f
Mateuss18 <mateusdfl18@gmail.com>
2025-03-30 15:15:55 -0300

    feat: toastification functions

--- src/main.js
+++ src/main.js
@@ -1,5 +1,27 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(Toast, {
+  position: "top-right",
+  timeout: 4000,
+  closeOnClick: true,
+  pauseOnFocusLoss: true,
+  pauseOnHover: true,
+  draggable: true,
+  draggablePercent: 0.6,
+  showCloseButtonOnHover: false,
+  hideProgressBar: false,
+  closeButton: "button",
+  icon: true,
+  rtl: false,
+  transition: "Vue-Toastification__bounce",
+  maxToasts: 3,
+  newestOnTop: true
+})
+
+app.mount('#app')
